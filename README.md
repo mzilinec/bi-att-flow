bd3208708cff4627124eb20ee24b9c2878e3addf
Your Name <ondrej.biza@gmail.com>
2018-03-30 22:55:10 +0200

    updated readme

--- README.md
+++ README.md
@@ -6,3 +6,18 @@
 
 2. Download pre-trained model from [this link](https://drive.google.com/drive/u/1/folders/12i0OV8Xq99KBRTbEa_ETnWIgLV0f9OVv)
 and extract it in the root directory of the project. You will need to move the folders so that you have ./out/basic/...
+
+3. (Optional) Put additional contexts in the ./data/contexts directory. Each context should be a JSON file with
+a "text" key that contains the context.
+
+## Usage ##
+
+Run the web server.
+```
+python run-webserver.py
+```
+
+Send a POST command with the context and the question in the body as a JSON.
+
+endpoint: **host:port/question**
+example body: { "context": "schmidhuber", "question": "Who is Jurgen Schmidhuber?" }
---
+++ run-webserver.py
@@ -0,0 +1,70 @@
+import os, json
+from flask import Flask, request, jsonify
+from squad.demo_prepro import prepro
+from basic.demo_cli import Demo
+
+
+# load contexts
+CONTEXTS_PATH = "./data/contexts"
+TEXT_KEY = "text"
+CONTEXT_KEY = "context"
+QUESTION_KEY = "question"
+
+if not os.path.isdir(CONTEXTS_PATH):
+  raise FileNotFoundError("Contexts directory not found.")
+
+context_files = os.listdir(CONTEXTS_PATH)
+
+if len(context_files) == 0:
+  raise FileNotFoundError("No contexts found.")
+
+contexts = {}
+for context_file in context_files:
+
+  context_path = os.path.join(CONTEXTS_PATH, context_file)
+  context_name = ".".join(context_file.split(".")[:-1])
+
+  with open(context_path, "r") as file:
+    context = json.load(file)
+
+  if TEXT_KEY not in context:
+    raise ValueError("Text not found in {:s}. Use \"{:s}\" key to supply text in the JSON file."
+                     .format(context_path, TEXT_KEY))
+
+  contexts[context_name] = context[TEXT_KEY]
+
+# setup flask app and the model
+app = Flask(__name__)
+demo = Demo()
+
+def getAnswer(paragraph, question):
+
+  pq_prepro = prepro(paragraph, question)
+
+  if len(pq_prepro['x']) > 1000:
+    return "[Error] Sorry, the number of words in paragraph cannot be more than 1000."
+
+  if len(pq_prepro['q']) > 100:
+    return "[Error] Sorry, the number of words in question cannot be more than 100."
+
+  return demo.run(pq_prepro)
+
+@app.route("/question", methods=["POST"])
+def submit():
+
+    body = request.get_json(silent=True)
+
+    question = body[QUESTION_KEY]
+    context = body[CONTEXT_KEY]
+
+    if context not in contexts:
+      return jsonify(result="[Error] Invalid context.")
+
+    answer = getAnswer(contexts[context], question)
+
+    return jsonify(result=answer)
+
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", port=1995, threaded=True)
